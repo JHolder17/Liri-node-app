11ed12065cd3be4ea7f3a4bdf81c236cd6a69c66
Joseph Holder <josephholder@Josephs-MacBook-Pro.local>
2018-04-21 14:19:58 -0400

    Most features

--- liri.js
+++ liri.js
@@ -1,32 +1,101 @@
-//read and set any environment variables with the dotenv package
 require("dotenv").config();
+var keys = require('./keys');
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var request = require('request');
+var fs = require('fs');
 
-//import the keys.js file and store it in a variable
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log(spotify);
+
 
 switch (process.argv[2]) {
     case "my-tweets":
-        var params = { JosephXholder: 'nodejs' };
+        var params = {
+            screen_name: 'JosephXholder',
+            count: 20
+        };
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
-            if (!error) {
-                console.log(tweets);
+            if (error) {
+                console.log("Error occurred: " + error);
             }
-            console.log(response);
+            for (i = 0; i < tweets.length; i++) {
+                console.log("JosephXholder: " + tweets[i].text + " Created: " + tweets[i].created_at);
+                console.log("--------------");
+            }
         });
         break;
     case "spotify-this-song":
-        spotify.search({ type: 'track', query: 'All the Small Things' }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            console.log(data);
-        });
+        var song = process.argv.slice(3).join(" ");
+        if (song !== undefined) {
+            spotify.search({ type: 'track', query: song }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                var info = data.tracks.items
+                console.log("\nArtist: " + info[0].artists[0].name +
+                    "\nSong title: " + info[0].name +
+                    "\nAlbum name: " + info[0].album.name +
+                    "\nURL Preview: " + info[0].preview_url);
+            });
+            //get working doesn't enter script 
+        } else {
+            spotify.search({ type: 'track', query: "The Sign" }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                var info = data.tracks.items
+                console.log("\nArtist: " + info[0].artists[0].name +
+                    "\nSong title: " + info[0].name +
+                    "\nAlbum name: " + info[0].album.name +
+                    "\nURL Preview: " + info[0].preview_url);
+            });
+        }
         break;
     case "movie-this":
+        var movie = process.argv.slice(3).join(" ");
+        if (movie !== undefined) {
+            var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+            request(queryUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log("*****");
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                    //need to access this rating
+                    console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings);
+                    console.log("Country of Production: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                    console.log("*****");
+                }
+            });
+        //get working doesn't enter script 
+        } else {
+            var queryUrl = "http://www.omdbapi.com/?t=Mr.+Nobody&y=&plot=short&apikey=trilogy";
+            request(queryUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    console.log("*****");
+                    console.log("Title: " + JSON.parse(body).Title);
+                    console.log("Release Year: " + JSON.parse(body).Year);
+                    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                    //need to access this rating
+                    console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings);
+                    console.log("Country of Production: " + JSON.parse(body).Country);
+                    console.log("Language: " + JSON.parse(body).Language);
+                    console.log("Plot: " + JSON.parse(body).Plot);
+                    console.log("Actors: " + JSON.parse(body).Actors);
+                    console.log("*****");
+                }
+            });
+        }
         break;
     case "do-what-it-says":
+        fs.readFile("random.txt", "utf8", function (error, action) {
+            console.log(action);
+        });
+
         break;
 }
